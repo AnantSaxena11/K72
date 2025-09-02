0da008b04842e34b55cd5f0a3c76292a0d94c54e
Anant Saxena <anantsaxena5454@gmail.com>
2025-09-02 23:08:05 +0530

    Routing done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import { Link, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
+import Agence from './pages/Agence'
+import Project from './pages/Projects'
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/agence' element={<Agence />} />
+        <Route path='/project' element={<Project />} />
+      </Routes>
+
+    </div>
   )
 }
 
---
+++ src/pages/Agence.tsx
@@ -0,0 +1,8 @@
+const Agence = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default Agence;
